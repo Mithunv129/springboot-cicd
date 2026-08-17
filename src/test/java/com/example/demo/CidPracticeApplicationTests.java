040e244fa71a449426868ab9b9cdc95ab91f86b9
Mithun Velusamy <mithun.velusamy@kumaran.com>
2026-08-17 16:15:18 +0530

    initial commit'

---
+++ src/test/java/com/example/demo/CidPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CidPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
